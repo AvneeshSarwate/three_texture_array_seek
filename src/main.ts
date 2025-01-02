88c59a9700b2ac8cb8768984737e7f94c24c271b
Avneesh Sarwate <avneeshsarwate@gmail.com>
2025-01-02 16:44:37 -0500

    more params to play with

--- src/main.ts
+++ src/main.ts
@@ -45,11 +45,11 @@ const init = async () => {
   uniforms = {
     frame: { value: 0 },
     textureArray: { value: null },
-    makeBlackThresh: { value: 2.7 },
+    makeBlackThresh: { value: 3 },
   };
 
   // Quad geometry and shader material
-  const size = 1.25
+  const size = 2
   const geometry = new THREE.PlaneGeometry(size, size);
   const material = new THREE.ShaderMaterial({
     uniforms,
@@ -74,23 +74,64 @@ const init = async () => {
     `,
   });
   material.transparent = true;
+  material.side = THREE.DoubleSide;
 
-  // quad = new THREE.Mesh(geometry, material);
-  // scene.add(quad);
-  //10 quads arranged left to right
+  const numQuads = 15
+  function linearPos() {
+    const positions = []
+    for (let i = 0; i < numQuads; i++) {
+      const pos = {
+        x: i * 0.4 - 2.75,
+        y: 0,
+      }
+      positions.push(pos)
+    }
+    return positions
+  }
+
+  function circlePos(time: number, radius: number) {
+    const positions = []
+    for (let i = 0; i < numQuads; i++) {
+      const pos = {
+        x: radius * Math.cos(i * 2 * Math.PI / numQuads + time),
+        y: radius * Math.sin(i * 2 * Math.PI / numQuads + time)
+      }
+      positions.push(pos)
+    }
+    return positions
+  }
+
+  function lerpPositionArrays(pos1: {x: number, y: number}[], pos2: {x: number, y: number}[], lerp: number) {
+    const positions = []
+    for (let i = 0; i < pos1.length; i++) {
+      const pos = {
+        x: lerp * pos1[i].x + (1 - lerp) * pos2[i].x,
+        y: lerp * pos1[i].y + (1 - lerp) * pos2[i].y,
+      }
+      positions.push(pos)
+    }
+    return positions
+  }
+
+
+
+
   const materials: THREE.ShaderMaterial[] = []
+  const meshes: THREE.Mesh[] = []
   const uniformsArr: { [key: string]: { value: any } }[] = []
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < numQuads; i++) {
     const matClone = material.clone()
     materials.push(matClone)
     uniformsArr.push({
       frame: { value: 0 },
       textureArray: { value: null },
-      makeBlackThresh: { value: 2.7 },
+      makeBlackThresh: { value: 3 },
     })
     matClone.uniforms = uniformsArr[i]
     const quad = new THREE.Mesh(geometry, matClone);
-    quad.position.x = i * 0.4 - 1.75;
+    quad.position.x = i * 0.4 - 2.75;
+    quad.position.z = i * 0.001
+    meshes.push(quad)
     scene.add(quad);
   }
 
@@ -105,7 +146,7 @@ const init = async () => {
   loader.load('tydance_540_texture_array.ktx2', (texureArray) => {
     console.log("texureArray", "megs", texureArray.mipmaps!![0].data.length / 1000000, "format", texureArray.format);
     uniforms.textureArray.value = texureArray;
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < numQuads; i++) {
       uniformsArr[i].textureArray.value = texureArray;
     }
   });
@@ -132,29 +173,51 @@ const init = async () => {
   // Slider
   const slider = document.querySelector<HTMLInputElement>("#frame-slider")!;
   slider.addEventListener("input", (event) => {
-    uniforms.frame.value = parseInt(
-      (event.target as HTMLInputElement).value,
-      10
-    );
-    console.log("frame", uniforms.frame.value);
-    frameSliderVal = uniforms.frame.value
+    frameSliderVal = parseInt((event.target as HTMLInputElement).value, 10);
   });
 
-  const makeBlackThreshSlider = document.querySelector<HTMLInputElement>("#make-black-thresh-slider")!;
-  makeBlackThreshSlider.addEventListener("input", (event) => {
-    uniforms.makeBlackThresh.value = parseFloat((event.target as HTMLInputElement).value);
+  let positionLerp = 0
+  const positionLerpSlider = document.querySelector<HTMLInputElement>("#position-lerp-slider")!;
+  positionLerpSlider.addEventListener("input", (event) => {
+    positionLerp = parseFloat((event.target as HTMLInputElement).value);
+  });
+
+  let circleSpeed = 0
+  const circleSpeedSlider = document.querySelector<HTMLInputElement>("#circle-speed-slider")!;
+  circleSpeedSlider.addEventListener("input", (event) => {
+    circleSpeed = parseFloat((event.target as HTMLInputElement).value);
+  });
+
+  const lookbackSlider = document.querySelector<HTMLInputElement>("#lookback-slider")!;
+  lookbackSlider.addEventListener("input", (event) => {
+    lookback = parseInt((event.target as HTMLInputElement).value);
   });
 
   // Render loop
   let frame = 0
+  let lookback = 20
+  let accumTime = 0
+  let lastTime = 0
   const animate = () => {
+    const newTime = performance.now()
+    const deltaTime = newTime - lastTime
+    lastTime = newTime
+    accumTime += deltaTime / 1000 * circleSpeed
+
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
     frame++
     circularBuffer.add(frameSliderVal)
-    for (let i = 0; i < 10; i++) {
-      uniformsArr[i].frame.value = circularBuffer.getNBack(i * 20);
-    }
+    uniformsArr.forEach((uniform, i) => {
+      uniform.frame.value = circularBuffer.getNBack(i * lookback)
+    })
+    const pos1 = linearPos()
+    const pos2 = circlePos(accumTime, 1.5)
+    const pos = lerpPositionArrays(pos2, pos1, positionLerp)
+    meshes.forEach((mesh, i) => {
+      mesh.position.copy({x: pos[i].x, y: pos[i].y, z: i * 0.001})
+    })
+
     // console.log('render')
   };
 
