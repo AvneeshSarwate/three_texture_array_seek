25ec48d71a4c5775690e9cb42085e5d548b61ce7
Avneesh Sarwate <avneeshsarwate@gmail.com>
2025-01-02 12:08:37 -0500

    transparency and circular buffer

--- src/main.ts
+++ src/main.ts
@@ -45,10 +45,12 @@ const init = async () => {
   uniforms = {
     frame: { value: 0 },
     textureArray: { value: null },
+    makeBlackThresh: { value: 2.7 },
   };
 
   // Quad geometry and shader material
-  const geometry = new THREE.PlaneGeometry(1.5, 1.5);
+  const size = 1.25
+  const geometry = new THREE.PlaneGeometry(size, size);
   const material = new THREE.ShaderMaterial({
     uniforms,
     vertexShader: `
@@ -61,17 +63,36 @@ const init = async () => {
     fragmentShader: `
       uniform sampler2DArray textureArray;
       uniform float frame;
+      uniform float makeBlackThresh;
       varying vec2 vUv;
       void main() {
         vec2 yFlip = vec2(vUv.x, 1.0 - vUv.y);
         vec4 color = texture(textureArray, vec3(yFlip, frame));
-        gl_FragColor = color.a > 0. ? color : vec4(0,0,0,1);
+        bool makeBlack = color.a == 0.0 || color.r + color.g + color.b > makeBlackThresh;
+        gl_FragColor = makeBlack ? vec4(0,0,0,0) : color;
       }
     `,
   });
+  material.transparent = true;
 
-  quad = new THREE.Mesh(geometry, material);
-  scene.add(quad);
+  // quad = new THREE.Mesh(geometry, material);
+  // scene.add(quad);
+  //10 quads arranged left to right
+  const materials: THREE.ShaderMaterial[] = []
+  const uniformsArr: { [key: string]: { value: any } }[] = []
+  for (let i = 0; i < 10; i++) {
+    const matClone = material.clone()
+    materials.push(matClone)
+    uniformsArr.push({
+      frame: { value: 0 },
+      textureArray: { value: null },
+      makeBlackThresh: { value: 2.7 },
+    })
+    matClone.uniforms = uniformsArr[i]
+    const quad = new THREE.Mesh(geometry, matClone);
+    quad.position.x = i * 0.4 - 1.75;
+    scene.add(quad);
+  }
 
   // Load textures
   const loader = new KTX2Loader()
@@ -84,8 +105,30 @@ const init = async () => {
   loader.load('tydance_540_texture_array.ktx2', (texureArray) => {
     console.log("texureArray", "megs", texureArray.mipmaps!![0].data.length / 1000000, "format", texureArray.format);
     uniforms.textureArray.value = texureArray;
+    for (let i = 0; i < 10; i++) {
+      uniformsArr[i].textureArray.value = texureArray;
+    }
   });
 
+  class CircularBuffer {
+    private buffer: any[] = [];
+    private size: number;
+    constructor(size: number) {
+      this.size = size;
+    }
+    add(item: any) {
+      this.buffer.push(item);
+      if (this.buffer.length > this.size) {
+        this.buffer.shift();
+      }
+    }
+    getNBack(n: number) {
+      return this.buffer[this.buffer.length - n - 1];
+    }
+  }
+
+  const circularBuffer = new CircularBuffer(600)
+  let frameSliderVal = 0
   // Slider
   const slider = document.querySelector<HTMLInputElement>("#frame-slider")!;
   slider.addEventListener("input", (event) => {
@@ -94,12 +137,24 @@ const init = async () => {
       10
     );
     console.log("frame", uniforms.frame.value);
+    frameSliderVal = uniforms.frame.value
+  });
+
+  const makeBlackThreshSlider = document.querySelector<HTMLInputElement>("#make-black-thresh-slider")!;
+  makeBlackThreshSlider.addEventListener("input", (event) => {
+    uniforms.makeBlackThresh.value = parseFloat((event.target as HTMLInputElement).value);
   });
 
   // Render loop
+  let frame = 0
   const animate = () => {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
+    frame++
+    circularBuffer.add(frameSliderVal)
+    for (let i = 0; i < 10; i++) {
+      uniformsArr[i].frame.value = circularBuffer.getNBack(i * 20);
+    }
     // console.log('render')
   };
 
