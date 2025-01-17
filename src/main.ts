7859246b6ad84a7edf7f60f02d1ba865e1cc569a
Avneesh Sarwate <avneesh@castle.xyz>
2025-01-17 00:37:12 -0800

    smoothing out bezier curves

--- src/main.ts
+++ src/main.ts
@@ -518,19 +518,44 @@ type Point = {
   x: number
   y: number
 }
+
+function distance(p1: Point, p2: Point) {
+  return Math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2)
+}
+
 const init3 = async () => {
 
   // const canvas = document.querySelector<HTMLCanvasElement>("#three-canvas")!;
 
   // const splineFrames = contours.diana.frames.map(frame => bezierToCatmullRomExact(frame))
   const people = ["aroma", "chloe", "chris", "diana", "idris", "iman", "jah", "jesse", "kat", "kurush", "latasha", "martin", "robert", "rupal", "sara", "segnon", "senay", "shreya", "stoney", "zandie"]
+
+  //if control pt 1 is more than threshold distance from anchor pt 1, 
+  //and control pt 2 is more than threshold distance from anchor pt 2,
+  //then replace control pts 1 and 2 with the avg of the anchor pts
+  function smoothBezierCurve(curve: number[], threshold: number) {
+    const [x1, y1, x2, y2, x3, y3, x4, y4] = curve
+    const anchor1 = {x: x1, y: y1}
+    const anchor2 = {x: x4, y: y4}
+    const control1 = {x: x2, y: y2}
+    const control2 = {x: x3, y: y3}
+    const dist1 = distance(anchor1, control1)
+    const dist2 = distance(anchor2, control2)
+    if(dist1 > threshold && dist2 > threshold) {
+      const avg = {x: (anchor1.x + anchor2.x) / 2, y: (anchor1.y + anchor2.y) / 2}
+      return [x1, y1, avg.x, avg.y, avg.x, avg.y, x4, y4]
+    }
+    return curve
+  }
   
   const countoursAndSkeletonForPerson = (person: string) => {
-    const bezierCurves = contours[person].frames
-    const splineFrames0 = contours[person].frames.map(frame => bez2CatmullSample(frame))
+    const bezierCurves = contours[person].frames.map(frame => {
+      return frame.map(curve => smoothBezierCurve(curve, 20))
+    })
+    const splineFrames0 = bezierCurves.map(frame => bez2CatmullSample(frame))
     const maxPoints = Math.max(...splineFrames0.map(frame => frame.length))
-    // const splineFrames = splineFrames0.map(frame => resampleSplineEquidistant(frame, maxPoints))
-    const splineFrames = splineFrames0
+    const splineFrames = splineFrames0.map(frame => resampleSplineEquidistant(frame, maxPoints))
+    // const splineFrames = splineFrames0
     const numFrames = Object.keys(skeletons.data[person]).length
     const onePersonSkeletons = Array(numFrames).fill(null).map((_, i) => skeletons.data[person][(i + 1).toString().padStart(6, '0')+'.png'])
     return {splineFrames, onePersonSkeletons, bezierCurves, numFrames}
@@ -588,26 +613,26 @@ const init3 = async () => {
       const frameCeil = Math.ceil(frameFrac) % splineFrames.length
       
 
-      const frame = bezierCurves[frameFloor]
-      frame.forEach(curve => {
-        const [x1, y1, x2, y2, x3, y3, x4, y4] = curve
-        p.bezier(x1, y1, x2, y2, x3, y3, x4, y4)
-      })
+      // const frame = bezierCurves[frameFloor]
+      // frame.forEach(curve => {
+      //   const [x1, y1, x2, y2, x3, y3, x4, y4] = curve
+      //   p.bezier(x1, y1, x2, y2, x3, y3, x4, y4)
+      // })
 
       //todo - need to rotationally reorient frames before lerping looks good
       // const lerpedFrame = lerpPoints(splineFrames[frameFloor], splineFrames[frameCeil], frameFrac % 1)
       // const framePts = lerpedFrame;
 
-      // const framePts = splineFrames[frameFloor]
-      // p.beginShape()
-      // framePts.forEach((point, i) => {
-      //   // p.curveVertex(point.x, point.y)
-      //   // p.vertex(point.x, point.y)
-      //   if(i > 0) {
-      //     p.line(point.x, point.y, framePts[i - 1].x, framePts[i - 1].y)
-      //   }
-      // })
-      // p.endShape()
+      const framePts = splineFrames[frameFloor]
+      p.beginShape()
+      framePts.forEach((point, i) => {
+        p.curveVertex(point.x, point.y)
+        // p.vertex(point.x, point.y)
+        // if(i > 0) {
+        //   p.line(point.x, point.y, framePts[i - 1].x, framePts[i - 1].y)
+        // }
+      })
+      p.endShape()
 
 
       const skeletonsInFrame = onePersonSkeletons[frameFloor]
